80770507fab3a28b4fa5d6704240c96bb7884baa
harmandeepkaler2000 <45006546+harmankaler2000@users.noreply.github.com>
2021-06-03 12:58:45 +0530

    floor and ceil functions

---
+++ floor_ceil.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"Floor is: "<<floor(2.3)<<"\n";
+    cout<<"Florr is: "<<floor(-2.3)<<"\n";
+
+    cout << " Ceil is : " << ceil(2.3) << endl;
+    cout << " Ceil is : " << ceil(-2.3) << endl;
+    return 0;
+}
