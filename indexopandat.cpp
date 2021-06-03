a982eaff5e5c9bd0d158720af13715e5933ff66a
harmandeepkaler2000 <45006546+harmankaler2000@users.noreply.github.com>
2021-06-03 13:01:40 +0530

    vector function

---
+++ indexopandat.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(1);
+    v.push_back(7);
+    v.push_back(3);
+    cout<<"Using the index operator: "<<v[3]<<endl;
+    cout<<"Using the at() function: "<<v.at(3)<<endl;
+    return 0;
+}
