6008464ccb16a9cdeee798b761509ace85529fc1
harmandeepkaler2000 <45006546+harmankaler2000@users.noreply.github.com>
2021-06-03 12:03:41 +0530

    2D vector

---
+++ 2d.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    //intialize the 2-D vector
+    vector<vector<int>> vect{{1,2,3},{4,5,6},{7,8,9}};
+    //displaying the 2-D vector
+    for(int i=0;i<vect.size();i++)
+    {
+        for(int j=0;j<vect[i].size();j++)
+        {
+            cout<<vect[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
